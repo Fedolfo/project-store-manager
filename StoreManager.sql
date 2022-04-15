a6fb7a3b18a82afbdd7452727db7567e0a38300d
Filipe <filipecandido123@hotmail.com>
2022-04-14 23:45:19 -0300

    feat: adiciona querys de insertação de valores no banco de dados

--- StoreManager.sql
+++ StoreManager.sql
@@ -21,3 +21,37 @@ CREATE TABLE sales_products (
 ) ENGINE = INNODB;
 SET
   SQL_SAFE_UPDATES = 0;
+INSERT INTO
+  StoreManager.products (name, quantity)
+VALUES
+  ('Bread', 10),
+  ('Chocolate', 20),
+  ('Coconut', 30),
+  ('Coffee', 40),
+  ('Cream', 50),
+  ('Eggs', 60),
+  ('Flour', 70),
+  ('Milk', 80),
+  ('Oil', 90),
+  ('Peanut Butter', 100),
+  ('Sugar', 110),
+  ('Tea', 120);
+INSERT INTO
+  StoreManager.sales (id)
+VALUES
+  (1);
+INSERT INTO
+  StoreManager.sales_products (sale_id, product_id, quantity)
+VALUES
+  (1, 1, 1),
+  (1, 2, 2),
+  (1, 3, 3),
+  (1, 4, 4),
+  (1, 5, 5),
+  (1, 6, 6),
+  (1, 7, 7),
+  (1, 8, 8),
+  (1, 9, 9),
+  (1, 10, 10),
+  (1, 11, 11),
+  (1, 12, 12);
